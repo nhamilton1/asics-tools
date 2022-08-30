e6f8f83ccaa604cba87b470ab3195508385ea243
Nicholas Hamilton <nickh3592@gmail.com>
2022-08-30 19:27:35 -0400

    testing dataunlocker, if this is even correct way to do it

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const { withPlausibleProxy } = require("next-plausible");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/trpc/*",
+        destination: `${process.env.DATAUNLOCKER_HOST}/*`, // Proxy to Backend
+      },
+    ];
+  },
 };
 
 module.exports = withPlausibleProxy()({ nextConfig });
